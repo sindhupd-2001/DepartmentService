48e89bd590f467e785b9e86aaf1ea52c74df3c34
sindhupd-2001 <98105619+sindhupd-2001@users.noreply.github.com>
2022-11-24 07:38:53 +0530

    Initial commit

---
+++ departmentservice/src/main/java/net/javaguides/departmentservice/entity/Department.java
@@ -0,0 +1,21 @@
+package net.javaguides.departmentservice.entity;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
---
+++ departmentservice/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,26 @@
+package net.javaguides.departmentservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javaguides.departmentservice.entity.Department;
+import net.javaguides.departmentservice.repository.DepartmentRepository;
+import net.javaguides.departmentservice.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    @Override
+    public Department getDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId).get();
+    }
+}
